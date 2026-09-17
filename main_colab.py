1232227d79a17de6e4f404ecff3a04634cb6a530
Darren Gavriel <darrengvs.dg@gmail.com>
2026-09-17 11:36:49 +0700

    add initial FastAPI application with ngrok integration and query processing

---
+++ main_colab.py
@@ -0,0 +1,34 @@
+import os
+import ngrok
+from fastapi import FastAPI
+from pydantic import BaseModel
+from contextlib import asynccontextmanager
+import hybrid_rag
+
+def connect_ngrok():
+    forwarder = ngrok.forward("localhost:8000", authtoken_from_env=True, domain="plating-ambition-mammal.ngrok-free.dev")
+    print(f"🌍 API kamu sudah online di: {forwarder.url()}")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    connect_ngrok()
+    yield 
+    ngrok.disconnect()
+
+app = FastAPI(lifespan=lifespan)
+
+class queryRequest(BaseModel):
+    query: str
+    turn: int
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/query")
+def process_query(query: queryRequest):
+    if query.turn == 0:
+        resultRag = hybrid_rag.main(query.query)
+        if resultRag[1] == False:
+            return {"query": query.query, "result": resultRag[0]}
+    return {"query": query}
