dd5b78f8c3fff729fe605bc9fa017a45452e1d33
Darren Gavriel <darrengvs.dg@gmail.com>
2026-09-18 10:35:16 +0700

    add install script for required using model llm

---
+++ install.sh
@@ -0,0 +1,4 @@
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
+pip install --no-deps xformers trl peft accelerate bitsandbytes
+pip install torchvision codecarbon
